6cbe429a61b474dc7ede748e7e43767d3dc4d4cd
alejandro-du <alejandro.d.a@gmail.com>
2022-02-24 19:12:50 +0200

    Use 1-thread pool

--- reactive-programming/src/main/java/com/example/Application.java
+++ reactive-programming/src/main/java/com/example/Application.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors;
 public class Application {
 
     public static void main(String[] args) {
-        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        ExecutorService threadPool = Executors.newFixedThreadPool(1);
 
         for (int u = 1; u <= 10; u++) {
             var user = "User " + u;
